37aaf1ca93eac6de23bde7236d20636379f7abf8
jalakamsairam <sairam.jalakamdevarajulu@mavs.uta.edu>
2017-10-24 14:16:33 -0700

    added routes for the app with dummy variables

--- server15.js
+++ server15.js
@@ -7,15 +7,34 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = 2343;
+
+//setting up arrays for reservations and waiting list
+// =============================================================
+var reservations = [
+  {
+    routeName:"reservation1",
+    name:"joseph",
+    phoneNumber:"4084089999",
+    email:"joseph.scubadiver@gmail.com",
+    uniqueId:1
+  }
+];
+var waitingList = [];
 
 // Sets up the Express app to handle data parsing
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+//setting up routes
+// =============================================================
 app.get("/", function(req, res) {
-  // res.sendFile(path.join(__dirname, "view.html"));
+ res.send("welcome to hot restaurants")
 });
+
+app.get("/reservations", function(req,res){
+  res.json(reservations[0]);
+})
 
 
 app.listen(PORT, function() {
